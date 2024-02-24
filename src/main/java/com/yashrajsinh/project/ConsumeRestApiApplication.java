4ae3470e0baf5d2e197967c2c721ce4c5b6e89d3
Yashrajsinhdev <yashrajsinhdev@gmail.com>
2024-02-24 23:53:48 +0530

    Adding all the Project Files

---
+++ src/main/java/com/yashrajsinh/project/ConsumeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.yashrajsinh.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeRestApiApplication.class, args);
+	}
+
+}
